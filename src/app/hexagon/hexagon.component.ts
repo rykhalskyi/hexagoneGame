0c3d60fc19636143611da75a0201e23b086e8e72
Jaroslav Rykhalskyi <jaroslav.rykhalskyi@amana.de>
2020-04-11 21:51:04 +0200

    Add hexagon values captions

--- src/app/hexagon/hexagon.component.ts
+++ src/app/hexagon/hexagon.component.ts
@@ -11,6 +11,9 @@ export class HexagonComponent implements OnInit {
   title = "hexagon";
   tockenFill : string[] = ["#fff","#f0f","#ff0","0ff","#fff","#f0f"];
   tockenTextFill : string[] = ["#000","#000","#000","000","#00","#000"];
+  tockenValue : string[][] = [["+1","+2"],["+3","+4"],["+5","+6"],["+7","+8"],["+9","+10"],["+11","+12"]];
+
+  value : string[] = ["+6","+16"];
 
   constructor() {
     this.color = "#a60709";
